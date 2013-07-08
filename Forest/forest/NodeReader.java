a018cb7f9be320f945bb9aea7379285ac643e457
yuta <g1145442@cc.kyoto-su.ac.jp>
2013-07-09 03:10:00 +0900

    更新

--- Forest/forest/NodeReader.java
+++ Forest/forest/NodeReader.java
@@ -1,18 +1,112 @@
 package Forest;
 
 import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.lang.StringBuilder;
 
-class NodeReader{
+class NodeReader extends Thread{
 	
 	private String text;	
-	private enum NodeState{}; 
+	private enum NodeState{INIT,INNUM,ENDNUM,INWORD,ERROR,END}; 
 	private HashMap<Integer,String> nodeMap;
 
 	public NodeReader(String str,HashMap<Integer,String> map){
-	
+		text = str;
+		nodeMap = map;
 	}
 
 	public void run(){
-	
+		int count=0,num=0,textLength = text.length();
+		String nodeWord;
+		Pattern number = Pattern.compile("[0-9]+");	
+		Pattern comma = Pattern.compile("[,]");	
+		Pattern space = Pattern.compile("[ ]");
+		Pattern enter = Pattern.compile("[\n]");
+		Pattern str = Pattern.compile("[[a-z]+|[A-Z]+]");
+		NodeState state = NodeState.INIT;
+
+		StringBuilder numBuilder = new StringBuilder();
+		StringBuilder wordBuilder = new StringBuilder();
+		
+		while(state != NodeState.END&&textLength>count){
+			char word = text.charAt(count);
+			Matcher n = number.matcher(""+word);
+			Matcher c = comma.matcher(""+word);
+			Matcher s = space.matcher(""+word);
+			Matcher e = enter.matcher(""+word);
+			Matcher w = str.matcher(""+word);
+			count++;
+			switch(state){
+				case INIT:
+					if(n.find()){
+						numBuilder.append(word);
+						state = NodeState.INNUM;
+					}
+					else{
+						System.out.println("INIT:"+word);
+						state = NodeState.ERROR;
+					}
+					break;
+				case INNUM:
+					if(count==textLength){
+						state = NodeState.END;
+					}
+					else if(n.find()){
+						numBuilder.append(word);
+					}
+					else if(c.find()){
+						num = toInteger(numBuilder.toString());
+						state = NodeState.ENDNUM;
+					}
+					else{
+						state = NodeState.ERROR;
+						System.out.println("INNUM:"+word);
+					}
+					break;
+				case ENDNUM:
+					if(s.find()){
+						//何もしない
+					}
+					else if(w.find()){
+						wordBuilder.append(word);
+						state = NodeState.INWORD;
+					}
+					else{
+						state = NodeState.ERROR;
+						System.out.println("ENDNUM:"+word);
+					}
+					break;
+				case INWORD:
+					if(w.find()){
+						wordBuilder.append(word);
+					}
+					else if(e.find()){
+						nodeWord = wordBuilder.toString();						
+						nodeMap.put(num,nodeWord);
+						//System.out.println(num+","+nodeWord);//でバッグ用
+						num=0;
+						nodeWord=null;
+						numBuilder = new StringBuilder();
+						wordBuilder = new StringBuilder();
+						state = NodeState.INNUM;
+						
+					}
+					else{
+						state = NodeState.ERROR;
+						System.out.println("INWORD:"+word);
+					}
+					break;
+				case ERROR:
+					System.out.println("error");
+					state = NodeState.END;
+					break;
+
+			}
+		}
+	}
+
+	int toInteger(String a){
+		return Integer.parseInt(a);
 	}
 }
--- Forest/forest/TreeReader.java
+++ Forest/forest/TreeReader.java
@@ -1,19 +1,135 @@
 package Forest;
 
-import java.util.HashMap;
+import java.lang.Thread;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
+import java.util.HashMap;
 
-class TreeReader{
-	
+class TreeReader extends Thread{
+
 	private String text;	
-	private enum TreeState{}; 
+	private enum TreeState{INIT,INWORD,MADENODE,INBRANCH,ONEHYPHEN,TWOHYPHEN,ERROR,ENDSTATE}; 
 	private HashMap<String,Integer> treeMap;
+	private ArrayList<Node> rootList;
 
-	public TreeReader(String str,HashMap<String,Integer> map){
-	
-	}
+	public TreeReader(String str,HashMap<String,Integer> map,ArrayList<Node> root){
+			treeMap = map;
+			rootList = root;
+			text =str;
+		}
 
 	public void run(){
-	
+		int depth=0,count=0,textLength = text.length();
+		Pattern hyphen = Pattern.compile("[-]");
+		Pattern space = Pattern.compile("[ ]");
+		Pattern vertical = Pattern.compile("[|]");
+		Pattern enter = Pattern.compile("[\n]");
+		Pattern str = Pattern.compile("[[a-z]+|[A-Z]+]");
+		TreeState state = TreeState.INIT;
+
+		StringBuilder wordBuilder = new StringBuilder();
+
+		while(state != TreeState.ENDSTATE&&textLength>count){
+			char word = text.charAt(count);
+			Matcher h = hyphen.matcher(""+word);
+			Matcher s = space.matcher(""+word);
+			Matcher e = enter.matcher(""+word);
+			Matcher w = str.matcher(""+word);
+			Matcher v = vertical.matcher(""+word);
+			count++;
+			switch(state){
+				case INIT:
+					if(w.find()){
+						wordBuilder.append(word);
+						state = TreeState.INWORD;
+					}
+					else{
+						System.out.println("INIT:"+word);
+					}
+					break;
+				case INWORD:
+					if(w.find()){
+						wordBuilder.append(word);
+					}
+					else if(e.find()&&depth==0){
+						Node node = new Node();					
+						rootList.add(node);
+						treeMap.put(wordBuilder.toString(),depth);
+						wordBuilder = new StringBuilder();
+						state = TreeState.MADENODE;
+					}
+					else if(e.find()&&depth!=0){
+						Node node = new Node();
+						treeMap.put(wordBuilder.toString(),depth);
+						wordBuilder = new StringBuilder();
+						depth=0;
+						state = TreeState.MADENODE;
+					}
+					else{
+						System.out.println("INWORD");
+						state = TreeState.ERROR;
+					}
+					break;
+				case MADENODE:
+					if(count == textLength){
+						state = TreeState.ENDSTATE;
+					}
+					else if(w.find()){
+						wordBuilder.append(word);
+						state = TreeState.INWORD;
+					}
+					else if(v.find()){
+						state = TreeState.INBRANCH;
+					}
+					else{
+						System.out.println("MADENODE:"+ word);
+						state = TreeState.ERROR;
+					}
+					break;
+				case INBRANCH:
+					if(h.find()){
+						state = TreeState.ONEHYPHEN;
+					}
+					else{
+						state = TreeState.ERROR;
+					}
+					break;
+				case ONEHYPHEN:
+					if(h.find()){
+						depth++;
+						state = TreeState.TWOHYPHEN;	
+					}
+					else{
+						System.out.println("ONEHYPHEN:"+word);
+						state = TreeState.ERROR;
+					}
+					break;
+				case TWOHYPHEN:
+					if(w.find()){
+						wordBuilder.append(word);
+						state = TreeState.INWORD;
+					}
+					else if(v.find()){
+						state = TreeState.INBRANCH;
+					}
+					else if(s.find()){
+						//何もしない
+					}
+					else{
+						System.out.println("TWOHYPHEN:"+word);
+						state = TreeState.ERROR;
+					}
+					break;
+				case ERROR:
+					System.out.println("error");
+					state = TreeState.ENDSTATE;
+					break;
+			}
+
+
+		}
+
 	}
 }
